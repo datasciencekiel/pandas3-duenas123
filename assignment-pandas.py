d3365c4dd6a28b51ea1dbd6cae1688c7f227910f
duenas123 <112696628+duenas123@users.noreply.github.com>
2022-09-02 11:10:05 +0000

    Update assignement

--- assignment-pandas.py
+++ assignment-pandas.py
@@ -1,7 +1,14 @@
 # Load required packages here
 
+import pandas as pd
+import seaborn as sns
+
 # Read csv file
 
+sns.get_dataset_names()
+df = sns.load_dataset('titanic')
+
 # Sort data by xyz
+df.head()
 
 # Create visualization of xyz
